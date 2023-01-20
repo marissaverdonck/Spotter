239912a94ffe147dc5ca0b6d96891e0325b5a542
marissaverdonck <verdonck.marissa@gmail.com>
2023-01-20 17:31:06 +0100

    feat:  image optimization

--- apps/app/src/components/circle-layout/result-content.tsx
+++ apps/app/src/components/circle-layout/result-content.tsx
@@ -1,5 +1,5 @@
 import { Circle, ImageWrapper, Leave, Twig, ResultText } from './styles';
-
+import Image from 'next/image';
 interface Props {
   result: number;
 }
@@ -7,14 +7,20 @@ interface Props {
 export const ResultContentWrapper = ({ result }: Props) => {
   return (
     <ImageWrapper>
-      <Twig src="/images/twig.png" alt="twig" />
+      <Twig>
+        <Image src="/images/twig.png" alt="twig" fill={true} />
+      </Twig>
       <Circle color="yellow">
         <ResultText>
           <h1>Gefeliciteerd</h1>
-          <p>{`Je hebt ${result} ${result === 1 ? 'vogel' : 'vogels'} herkend uit de categorie tuinvogels`}</p>
+          <p>{`Je hebt ${result} ${
+            result === 1 ? 'vogel' : 'vogels'
+          } herkend uit de categorie tuinvogels`}</p>
         </ResultText>
       </Circle>
-      <Leave src="/images/leave.png" alt="leave" />
+      <Leave>
+        <Image src="/images/leave.png" alt="leave" fill={true} />
+      </Leave>
     </ImageWrapper>
   );
 };
